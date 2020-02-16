c592723218f1c0843a081fa9b9a119760ba8b5e5
PrinceNorbi <norbeey@gmail.com>
2020-02-16 13:46:47 +0100

    added TLS

--- flaskAppWrapper.sh
+++ flaskAppWrapper.sh
@@ -1,3 +1,3 @@
 export FLASK_APP=/home/pi/git/flaskApp/flaskTestApp1.py
 export FLASK_ENV=development
-flask run --host=0.0.0.0
+flask run --host=0.0.0.0 --cert=/etc/letsencrypt/live/ad-astra.hu/fullchain.pem --key=/etc/letsencrypt/live/ad-astra.hu/privkey.pem
